2fc7637d1f415051eb6be51bb1a4d2b71bb70253
Cauã Pereira <cauapereira_@hotmail.com>
2025-12-10 20:01:41 -0300

    feat: add task handlers to TaskController
    - newTask
    - getTasks
    - deleteTask
    - updateTask
    - doneTask

--- src/Controllers/TaskController.ts
+++ src/Controllers/TaskController.ts
@@ -1,18 +1,96 @@
 import Task from "../Models/MongoTask";
 import { Request, Response } from "express";
+import * as TaskServices from "../services/TaskService";
 import jwt from "jsonwebtoken";
 import { isUserOwner } from "../Handlers/handleIsUserOwner";
-import { increaseUserPoints } from "../helpers/RewardSystem";
 
-export const newTask = async (req: Request, res: Response) => {};
+import { UnauthorizedException } from "../exceptions/Unauthorized";
 
-export const getTasks = async (req: Request, res: Response) => {};
+export const newTask = async (req: Request, res: Response) => {
+  try {
+    const { task, description } = req.body;
+    const token = req.cookies.token;
+    const NewTask = await TaskServices.newTask({ task, description, token });
 
-export const deleteTask = async (req: Request, res: Response) => {};
+    res.status(201).json({ message: "Task created successfully", NewTask });
+  } catch (error: any) {
+    res
+      .status(error.status || 400)
+      .send({ error: error.name, message: error.message });
+  }
+};
 
-export const updateTask = async (req: Request, res: Response) => {};
+export const getTasks = async (req: Request, res: Response) => {
+  try {
+    const Task = TaskServices.getTasks(req.cookies.token);
 
-export const doneTask = async (req: Request, res: Response) => {};
+    res.status(200).send(Task);
+  } catch (error: any) {
+    res
+      .status(error.status || 400)
+      .send({ error: error.name, message: error.message });
+  }
+};
+
+export const deleteTask = async (req: Request, res: Response) => {
+  try {
+    const FindTask = await Task.findOne({ _id: req.params.taskId });
+    const checkIn = isUserOwner(req, FindTask?.userId.toString());
+    if (!checkIn)
+      return res
+        .status(401)
+        .send({ message: "Don't have enough permission to delete" });
+    const deletedTask = await TaskServices.deleteTask(req.params.taskId);
+
+    res.status(200).send({ message: "Task deleted successfully", deletedTask });
+  } catch (error: any) {
+    res
+      .status(error.status || 400)
+      .send({ error: error.name, message: error.message });
+  }
+};
+
+export const updateTask = async (req: Request, res: Response) => {
+  try {
+    const { task, description } = req.body;
+
+    const FindTask = await Task.findOne({ _id: req.params.taskId });
+    const checkIn = isUserOwner(req, FindTask?.userId.toString());
+    if (!checkIn)
+      throw new UnauthorizedException("Don`t have enough permission", 401);
+
+    const updatingTask = await TaskServices.updateTask(
+      req.params.taskId,
+      task,
+      description
+    );
+
+    res
+      .status(201)
+      .send({ message: "Task updated successfully", updatingTask });
+  } catch (error: any) {
+    res
+      .status(error.status || 400)
+      .send({ error: error.name, message: error.message });
+  }
+};
+
+export const doneTask = async (req: Request, res: Response) => {
+  try {
+    const FindTask = await Task.findOne({ _id: req.params.taskId });
+    const checkIn = isUserOwner(req, FindTask?.userId.toString());
+    if (!checkIn)
+      throw new UnauthorizedException("Don`t have enough permission", 401);
+
+    const doneTask = TaskServices.doneTask(req.params.taskId);
+
+    res.status(201).send({ message: "Task done successfully", doneTask });
+  } catch (error: any) {
+    res
+      .status(error.status || 400)
+      .send({ error: error.name, message: error.message });
+  }
+};
 
 export const countUserCompletedTasks = async (
   req: Request,
